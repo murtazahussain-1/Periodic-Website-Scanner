27523415931424d0e8d407015e0f3d9423aa268a
Khaleel A <khaleel.org@gmail.com>
2024-05-18 22:09:35 +0500

    Update script.py
    Bug fixed

--- script.py
+++ script.py
@@ -10,35 +10,37 @@ from email.mime.text import MIMEText
 import datetime
 
 def check_website(url, search_text):
+    afile = open("useragents.txt")
+    headers = random_line(afile).rstrip()  # Random header for bypassing security checks on the website
+    print(f"Header is: {headers}")
     try:
-        headers = random_line(afile).rstrip()  # Random header for bypassing security checks on the website
-        print(f"Header is: {headers}")
-
         response = requests.get(url, headers={'User-Agent': headers}, timeout=100)
+        # print(f"Below is response error if exists ---  0000  ----")
         response.raise_for_status()
 
         # Parse the HTML content of the page
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Search for the desired text in the parsed HTML (case-insensitive)
+        # print(soup.get_text().lower())
         if search_text.lower() in soup.get_text().lower():
             return True
         else:
-            print("not found")
+            print(f"{search_text} is not found ...")
     except Exception as e:
         print(f"Error: {e}")
     return False
 
 def send_email(subject, body):
     try:
-        sender_email = "sender@gmail.com"
+        sender_email = "khaleel.org@gmail.com"
         receiver_email = [
-            "receiver1@gmail.com",
-            "receiver2@gmail.com",
-            "receiver3@gmail.com",
-            "receiver4@gmail.com"
+            "khaleel.org@gmail.com",
+            "hk.jigar@gmail.com",
+            "tmail.khalil@gmail.com",
+            "khaleel.gcloud@gmail.com"
         ]
-        password = "abcd efgh ijkl mnop" # gmail app password, guide: https://support.google.com/mail/answer/185833?hl=en
+        password = "gdkp dcjy ycmr qqrz"
 
         # Set up the MIME
         message = MIMEMultipart()
@@ -75,12 +77,10 @@ def random_line(afile):
     lines = afile.readlines()
     return random.choice(lines)
 
-afile = open("useragents.txt")
-
 def main():
     # url = "https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=isla&realmId=108&categoryId=1600"  # Replace with the URL of the website you want to monitor
     # url = "https://web.archive.org/web/20230205081917/https://service2.diplo.de/rktermin/extern/choose_categoryList.do?locationCode=isla&realmId=108"
-    url = "https://service2.diplo.de/rktermin/extern/choose_categoryList.do?locationCode=isla&realmId=108"
+    url = "https://service2.diplo.de/rktermin/extern/choose_categoryList.do?locationCode=isla&realmId=108&request_locale=en" # main visa appointment page
     search_text = "study"  # Replace with the text you want to search for
     while True:
         print(f"Checking for = {search_text}")
@@ -91,7 +91,7 @@ def main():
                 time.sleep(5)
 
         # Check the website every 30 seconds (adjust as needed)
-        print("Going to sleep for 5 seconds")
+        print("\nGoing to sleep for 5 seconds")
         time.sleep(5)
 
 if __name__ == "__main__":
